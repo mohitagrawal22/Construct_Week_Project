f72fe7426a3e4198cf528dfb6f96df621304678c
kosireddisreeram <78956214+kosireddisreeram@users.noreply.github.com>
2022-02-25 02:14:03 +0530

    selfcareessentialpage1

--- selfcareessential.js
+++ selfcareessential.js
@@ -1,61 +1,61 @@
 data=[
-    {image:"https://www.rodanandfields.com/en-us/medias/AARMF50-cat.jpg?context=bWFzdGVyfHJvb3R8MTUyMTd8aW1hZ2UvanBlZ3xoYTUvaDliLzEzNjE1MDM2MTcwMjcwLmpwZ3xhM2U4ZGUyYzhhOGNhOTBiM2I0NjE4YjU1MTM5MjUwMGVhOTJhZjdlNzcwOTU2NmEwMDEwZjNmNmEyNDc1OGUz",
-url:"https://www.rodanandfields.com/en-us/shop/redefine-rejuvenation-mask/p/AARMF50",
-text:"REDEFINE Rejuvenation Mask",
-price:"$60.00 |  $54.00 "
+    {image:"https://www.rodanandfields.com/en-us/medias/HAAGRTRF-AAWA125-AATN125-AATL030-AAPL030-ENFM030-720x600-desktop-1.jpg?context=bWFzdGVyfGltYWdlc3w4NDEwM3xpbWFnZS9qcGVnfGltYWdlcy9oZGMvaGRkLzEzOTc3NjUzNTQyOTQyLmpwZ3xhN2M4ZTcyYjQxNzhjMjcyNDZiNzk5OTEzOWZhZDA2NzIyNzkyODhiYzY0NjY2ZTk1ZGY4OWQ0NTJjNjc3Njk0",
+url:"https://www.rodanandfields.com/en-us/shop/redefine-total-rf-serum-special/p/HAAGRTRF",
+text:"REDEFINE + TOtal RF Serum Special ",
+price:"$358.00 |  $322.00 "
 },
 {
-    image:"https://www.rodanandfields.com/en-us/medias/SORMF50-cat.jpg?context=bWFzdGVyfHJvb3R8MTg4MzN8aW1hZ2UvanBlZ3xoOTEvaDUzLzEzNjE1ODc4Mjc1MTAyLmpwZ3wwY2QzZDVmYTIzNDhlYjY1YjMzYWRjN2RkMzJmNzVlNGQxZWM1MTBhZjA1YjgwMzhkMWI4ZTNhMjM5ODRhMmJi",
-    url:"https://www.rodanandfields.com/en-us/shop/soothe-rescue-mask/p/SORMF50",
-    text:"Soothe Rescue Mask",
-    price:"$60.00 |  $54.00"
+    image:"https://www.rodanandfields.com/en-us/medias/HRVRGTRF-720x600-desktop-1.jpg?context=bWFzdGVyfGltYWdlc3w5NDMwOHxpbWFnZS9qcGVnfGltYWdlcy9oYzIvaGMzLzEzOTc3NjU2NTI0ODMwLmpwZ3wzZDI3Mzk1ZTZhMjVjMTMwMWZhMTYzNTNmZjVlYTAxM2YwMzYwYTg5OWU1NTNlYjY3MTYwNDQzNGY3ZTA4ZGY3",
+    url:"https://www.rodanandfields.com/en-us/shop/reverse-total-rf-serum-special/p/HRVRGTRF",
+    text:"REVERSE + Total Serum Special",
+    price:"$334.00 |  $300.00"
 },
 {
-    image:"https://www.rodanandfields.com/en-us/medias/UNRMF50-720x600-desktop-1.jpg?context=bWFzdGVyfGltYWdlc3w0MTg2NnxpbWFnZS9qcGVnfGltYWdlcy9oN2MvaDk2LzEzNjE1ODgxMzg4MDYyLmpwZ3w0ZGE1ZWNiZWI1OTdiYmFkMjY3ZWU3ZWVmMDBkZjdlOWYzMDY4MGUwYWE4NTZiZTZjN2M3YjUxN2M2NmYxYjhk",
+    image:"https://www.rodanandfields.com/en-us/medias/HUNRGTRF-720x600-desktop-1.jpg?context=bWFzdGVyfGltYWdlc3w5NTM5MnxpbWFnZS9qcGVnfGltYWdlcy9oY2YvaDhjLzEzOTc3NjU3OTY2NjIyLmpwZ3xkYzVkYmZhM2IyYmU1ZmFiZWI5NTMxYjQxYzhkZDNlNDMxMDYzNmFiZDg5NzUzNTc5NDI4NDllYWY0YzEzNmIz",
     url:"https://www.rodanandfields.com/en-us/shop/unblemish-clarifying-mask/p/UNRMF50",
     text:"UNBLEMISH Clarifying Mask",
-    price:"$60.00 |  $54.00 "
+    price:"$328.00 |  $295.00 "
 },
 {
-    image:"https://www.rodanandfields.com/en-us/medias/AAAPS01-Regimen-Explainer-AMP-MD-System-570x570-Desktop.png?context=bWFzdGVyfGltYWdlc3wxODcyNnxpbWFnZS9wbmd8aW1hZ2VzL2gzNS9oNDEvMTQ2MDQwOTQ0MDY2ODYucG5nfGYyNGNkZTQ1ODc2Zjg3OGYzMjZjNDFmMTI4NTdmNzNkOWM0MmIzZTZhMzRkMDE0OTMwNjRkOTZiMDdiZTYyMjk",
-    url:"https://www.rodanandfields.com/en-us/shop/redefine-amp-md-system/p/AAAPS01",
-    text:"REDEFINE AMP MD System",
-    price:"$227.00 |  $204.00 "
-},
-
-{
-image:"https://www.rodanandfields.com/en-us/medias/XTPCS01-Regimen-Explainer-Pore-Cleansing-MD-System-570x570-Desktop.png?context=bWFzdGVyfGltYWdlc3wyNTc5NXxpbWFnZS9wbmd8aW1hZ2VzL2gyOC9oMjkvMTQ2MDQwOTU3ODI5NDIucG5nfGYxY2EwZWMyODQxNTM0MTE5ODRmOTlkZjZmYTY5MzE2MDZjNjE5NmZmMjE4M2U1NTNmODE3ZDJhMWI3ZTM1Y2E",
-url:"https://www.rodanandfields.com/en-us/shop/pore-cleansing-md-system/p/XTPCS01",
-text:"Pore Cleansing MD System",
-price:"$260.00 |  $234.00"
+    image:"https://www.rodanandfields.com/en-us/medias/HSORGTRF-SOWA125-SOTT050-SOMO050-SOSS050-ENFM030-900x900-tablet-1.jpg?context=bWFzdGVyfGltYWdlc3wxMTAzNjF8aW1hZ2UvanBlZ3xpbWFnZXMvaGY3L2g5MS8xMzk3NzY1Nzc3MDAxNC5qcGd8ZDhkOGZhNDhhNjAwMTdjN2YxNmRhMjlkYjkzZmVjYzgyMTcwY2VhMmNlMjhlZDQ5NGJhMzExNWQxNjljYjM0Mg",
+    url:"https://www.rodanandfields.com/en-us/shop/soothe-total-rf-serum-special/p/HSORGTRF",
+    text:"SOOTHE + Total RF Serum Special",
+    price:"$323.00 |  $290.00  "
 },
 
 {
-    image:"https://www.rodanandfields.com/en-us/medias/ENHEFG1-570x570-Desktop.png?context=bWFzdGVyfGltYWdlc3wyMjI3N3xpbWFnZS9wbmd8aW1hZ2VzL2hkMS9oM2MvMTQ1MzIxNjY2MTUwNzAucG5nfGZlMDQ0NDRlMjczNDFlZjdkYWVmNjkwZWY4MDAwM2I4MjMwZGZiMzU3ZTViZGNlYjllNzBiZjAwY2FlZDU5NzA",
-    url:"https://www.rodanandfields.com/en-us/shop/active-hydration-serum/p/ENHEFG1",
-    text:"Active Hydration Serum",
-    price:"$112.00 |  $100.00 "
+image:"https://www.rodanandfields.com/en-us/medias/HAAGRLP1-AAWA125-AATN125-AATT030-AAPM030-ENHLSH01-720x600-desktop-1.jpg?context=bWFzdGVyfGltYWdlc3w0OTY1NnxpbWFnZS9qcGVnfGltYWdlcy9oY2IvaDg4LzEyNzgwOTE0NTA3ODA2LmpwZ3xlOGNmOTZiYjIyZGIxOTk4NGFhMDJiNDY1YzM0NzNjZjg2NDZiZTBkZTkwN2ZiOGIxZTBhNDQ0OWI5NzQ2ODMw",
+url:"https://www.rodanandfields.com/en-us/shop/recharge-total-rf-serum-special/p/HRCRGTRF",
+text:"RECHARGE + Total RF Serum Special",
+price:"$272.00 |  $244.00"
+},
+
+{
+    image:"https://www.rodanandfields.com/en-us/medias/HSORGTRF-SOWA125-SOTT050-SOMO050-SOSS050-ENFM030-900x900-tablet-1.jpg?context=bWFzdGVyfGltYWdlc3wxMTAzNjF8aW1hZ2UvanBlZ3xpbWFnZXMvaGY3L2g5MS8xMzk3NzY1Nzc3MDAxNC5qcGd8ZDhkOGZhNDhhNjAwMTdjN2YxNmRhMjlkYjkzZmVjYzgyMTcwY2VhMmNlMjhlZDQ5NGJhMzExNWQxNjljYjM0Mg",
+    url:"https://www.rodanandfields.com/en-us/shop/soothe-total-rf-serum-special/p/HSORGTRF",
+    text:"Soothe +TOtal RF Serum Special",
+    price:"$323.00 |  $290.00 "
 },
 
 {
     image:"https://www.rodanandfields.com/en-us/medias/RCSP040-720x600-desktop.jpg?context=bWFzdGVyfGltYWdlc3wxMjgxOXxpbWFnZS9qcGVnfGltYWdlcy9oNjEvaDIyLzkxNzEyNDIwMjQ5OTAuanBnfDIyNmU3MTAxZjRiOWY5YjFiZjEzNGZkNDhlOGQ0M2EyYzFhOGFmYzI2ODAzNjc5NjY5NzI4OWVjZmQyNjljOWU",
     url:"https://www.rodanandfields.com/en-us/shop/recharge-protect-blur-broad-spectrum-spf-30-sunscreen/p/RCSP040",
     text:"RECHARGE Protect + BLUR BROAD ",
+    price:"$323.00 |  $290.00"
+},
+{
+    image:"https://www.rodanandfields.com/en-us/medias/HRVRBLP1-RVWA125-RVTG125-RVTTG50-RVSS050-ENHLSH01-720x600-desktop-1.jpg?context=bWFzdGVyfGltYWdlc3w0ODU4NnxpbWFnZS9qcGVnfGltYWdlcy9oYmQvaDYzLzEyNzcxNTgxNzIyNjU0LmpwZ3wzZGRiMDk0ZDAzZjgzOTk1MThkZWEzNGRkOTVjNTk2NTc2MTJiYWY1ODk0Y2YwMmI2NTU2YzVkODgzYTA0YTFk",
+    url:"https://www.rodanandfields.com/en-us/shop/radiant-defense-perfecting-liquid-spf-30/p/DERDS",
+    text:"",
     price:"$48.00 |  $43.00"
+
 },
 // {
-//     image:"https://www.rodanandfields.com/en-us/medias/ENHEFG1-570x570-Desktop.png?context=bWFzdGVyfGltYWdlc3wyMjI3N3xpbWFnZS9wbmd8aW1hZ2VzL2hkMS9oM2MvMTQ1MzIxNjY2MTUwNzAucG5nfGZlMDQ0NDRlMjczNDFlZjdkYWVmNjkwZWY4MDAwM2I4MjMwZGZiMzU3ZTViZGNlYjllNzBiZjAwY2FlZDU5NzA",
-//     url:"https://www.rodanandfields.com/en-us/shop/radiant-defense-perfecting-liquid-spf-30/p/DERDS",
-//     text:"",
-//     price:"$48.00 |  $43.00"
-
-// }
-{
-    image:"https://www.rodanandfields.com/en-us/medias/RCSP040-720x600-desktop.jpg?context=bWFzdGVyfGltYWdlc3wxMjgxOXxpbWFnZS9qcGVnfGltYWdlcy9oNjEvaDIyLzkxNzEyNDIwMjQ5OTAuanBnfDIyNmU3MTAxZjRiOWY5YjFiZjEzNGZkNDhlOGQ0M2EyYzFhOGFmYzI2ODAzNjc5NjY5NzI4OWVjZmQyNjljOWU",
-    url:"https://www.rodanandfields.com/en-us/shop/recharge-protect-blur-broad-spectrum-spf-30-sunscreen/p/RCSP040",
-    text:"Radiant Defense Perfecting Liquid SPF",
-    price:"$48.00 |  $43.00"
-},
+//     image:"",
+//     url:"https://www.rodanandfields.com/en-us/shop/recharge-protect-blur-broad-spectrum-spf-30-sunscreen/p/RCSP040",
+//     text:"Radiant Defense Perfecting Liquid SPF",
+//     price:"$272.00 |  $244.00"
+// },
 {image:"https://www.rodanandfields.com/en-us/medias/ENBR001-720x600-DESKTOP.jpg?context=bWFzdGVyfHJvb3R8MTg2NDR8aW1hZ2UvanBlZ3xoNWMvaGQ5LzEzNTMzMTYzMDk0MDQ2LmpwZ3xiNmNjNjE3YmJmOTI4Y2UzZjAwM2RkMDQwZDNkNmNiZDQyZWQzMzQ4N2ZjZmMxOWY1OGEzYjQ1MTFiM2M1MGE5",
 url:"https://www.rodanandfields.com/en-us/shop/pore-cleansing-md-system/p/XTPCS01",
 text:"Pore Cleansing MD System",
